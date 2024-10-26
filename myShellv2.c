833cb6460228eedb65ae9ab0946cf00f10c9a9ba
Umar <hafizmmustafa3@gamil.com>
2024-10-26 22:55:57 +0500

    shell version 2 updated

--- myShellv2.c
+++ myShellv2.c
@@ -56,11 +56,107 @@ int execute(char *arglist[], int input_fd, int output_fd) {
         }
 
         execvp(arglist[0], arglist);
-        perror("!...command not found...!");
+        perror("command not found...!");
         exit(1);
     } else {
         int status;
         waitpid(cpid, &status, 0);
         return status;
     }
+}
+
+// Tokenize the command line input into arguments
+char **tokenize(char *cmdline) {
+    char **arglist = (char **)malloc(sizeof(char *) * (MAXARGS + 1));
+    for (int j = 0; j < MAXARGS + 1; j++) {
+        arglist[j] = (char *)malloc(sizeof(char) * ARGLEN);
+        bzero(arglist[j], ARGLEN);
+    }
+    if (cmdline[0] == '\0')
+        return NULL;
+
+    int argnum = 0;
+    char *cp = cmdline;
+    char *start;
+    int len;
+
+    while (*cp != '\0') {
+        while (*cp == ' ' || *cp == '\t')
+            cp++;
+        start = cp;
+        len = 1;
+        while (*++cp != '\0' && *cp != ' ' && *cp != '\t')
+            len++;
+        strncpy(arglist[argnum], start, len);
+        arglist[argnum][len] = '\0';
+        argnum++;
+    }
+    arglist[argnum] = NULL;
+    return arglist;
+}
+
+// Read input command
+char *read_cmd(char *prompt, FILE *fp) {
+    printf("%s", prompt);
+    int c;
+    int pos = 0;
+    char *cmdline = (char *)malloc(sizeof(char) * MAX_LEN);
+    while ((c = getc(fp)) != EOF) {
+        if (c == '\n')
+            break;
+        cmdline[pos++] = c;
+    }
+    if (c == EOF && pos == 0)
+        return NULL;
+    cmdline[pos] = '\0';
+    return cmdline;
+}
+
+// Handle pipes and redirection logic
+void handle_pipes_and_execute(char **arglist) {
+    int input_fd = -1, output_fd = -1;
+    int pipefd[2];
+    int has_pipe = 0;
+
+    // Look for I/O redirection or pipes
+    for (int i = 0; arglist[i] != NULL; i++) {
+        if (strcmp(arglist[i], "<") == 0) {
+            // Input redirection
+            input_fd = open(arglist[i + 1], O_RDONLY);
+            if (input_fd < 0) {
+                perror("Failed to open input file");
+                return;
+            }
+            free(arglist[i]);
+            arglist[i] = NULL;  // Terminate the argument list here
+        } else if (strcmp(arglist[i], ">") == 0) {
+            // Output redirection
+            output_fd = open(arglist[i + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (output_fd < 0) {
+                perror("Failed to open output file");
+                return;
+            }
+            free(arglist[i]);
+            arglist[i] = NULL;  // Terminate the argument list here
+        } else if (strcmp(arglist[i], "|") == 0) {
+            // Pipe handling
+            has_pipe = 1;
+            pipe(pipefd);
+
+            arglist[i] = NULL;  // Separate the first command
+            execute(arglist, input_fd, pipefd[1]);
+            // Close the write end
+            close(pipefd[1]);
+            // The next command reads from the pipe
+            input_fd = pipefd[0];
+            arglist = &arglist[i + 1];  
+            i = -1;  
+        }
+    }
+
+    // Execute the final command with possible redirection
+    execute(arglist, input_fd, output_fd);
+
+    if (input_fd != -1) close(input_fd);
+    if (output_fd != -1) close(output_fd);
 }
